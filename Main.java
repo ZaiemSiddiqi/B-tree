9b0324c527acb36afe0d368e5dfb49d8d51a5cc2
Si Chen <quake0day@gmail.com>
2022-04-19 07:50:34 -0400

    add test case

--- Main.java
+++ Main.java
@@ -1,14 +1,15 @@
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
-        BTree<Integer> bt = new BTree<>(5);
+	     // write your code here
+        BTree<Integer> bt = new BTree<>(4);
         bt.add(67);
         bt.add(77);
         bt.add(87);
         bt.add(97);
-        System.out.println(bt.getRoot());
-        bt.printLeaf();
+        System.out.println(bt.getRoot()); // 87
+        bt.printLeaf(); // 67,77,->87,97,->
+
     }
 }
 
